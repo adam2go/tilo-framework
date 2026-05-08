66ff5130329bb95089d20fbd340fb90da916fca0
adam2go <leeapply@163.com>
2026-05-08 22:48:20 +0800

    Apply cowork demo styles to demo route

--- frontend/app/demo/page.tsx
+++ frontend/app/demo/page.tsx
@@ -1,5 +1,11 @@
 import { MinimalDemoPage } from "../../components/demo-minimal/MinimalDemoPage";
+import { CoworkDemoStyles } from "../../components/demo-minimal/CoworkDemoStyles";
 
 export default function DemoRoute() {
-  return <MinimalDemoPage />;
+  return (
+    <>
+      <CoworkDemoStyles />
+      <MinimalDemoPage />
+    </>
+  );
 }
